61e04cda20e0224b326f529d298b5dfd3a378930
moontacer <montasserthegangsta@gmail.com>
2022-06-20 00:52:49 +0100

    animation commit

--- js/main.js
+++ js/main.js
@@ -30,9 +30,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.png': 'center',
-							'images/bg03.jpg': 'center',
-							'images/bg02.jpg': 'center'
+							'../images/bg01.png': 'center',
+							'../images/bg03.jpg': 'center',
+							'../images/bg02.jpg': 'center'
 						},
 
 					// Delay.
